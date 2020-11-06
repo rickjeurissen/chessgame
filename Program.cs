efa39e74dcd5136faa1e1ed460ae5d762ca10b99
Rick Jeurissen <Rick.Jeurissen@centric.eu>
2020-11-06 23:49:10 +0100

    Representation of board done

--- Program.cs
+++ Program.cs
@@ -1,15 +1,44 @@
-﻿using System;
+﻿using chessgame.engine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace chessgame
+
+namespace chessgame.console
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("============ Welcome to chessgame! ============");
+            Console.WriteLine("");
+            Console.WriteLine("The units of Player 1 are uppercase letters.");
+            Console.WriteLine("The units of Player 2 are lowecase letters.");
+            Console.WriteLine("Each character represents the following unit:");
+            Console.WriteLine("  R: Rook");
+            Console.WriteLine("  H: Horse");
+            Console.WriteLine("  B: Bishop");
+            Console.WriteLine("  Q: Queen");
+            Console.WriteLine("  K: King");
+            Console.WriteLine("  P: Pawn");
+            Console.WriteLine("  .: Nothing");
+            Console.WriteLine("");
+            Console.WriteLine("===============================================");
+            Console.WriteLine("");
+
+            while (true)
+            {
+                Board board = new Board();
+
+                board.InitializeGame();
+                
+                Console.WriteLine(board);
+                Console.WriteLine("");
+                Console.Write("Player 1's turn. Move: ");
+                Console.ReadLine();
+            }
         }
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("chessgame")]
+[assembly: AssemblyTitle("chessgame.console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("chessgame")]
+[assembly: AssemblyProduct("chessgame.console")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
